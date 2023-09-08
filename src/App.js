046dad774bee1d4bc3328f35d019e9527d8fc555
unknown <inaiyokuro1724@gmail.com>
2023-09-09 02:35:32 +0700

    five

--- src/App.js
+++ src/App.js
@@ -7,15 +7,13 @@ import Register from './components/Register';
 
 function App() {
   return (
-    <Login />
-  //   <BrowserRouter>
-  //   <div>Hello World</div>
-  //   <Routes>
-  //     <Route path="/" element={<ChatRoom />}></Route>
-  //     <Route path="/login" element={<Login />}></Route>
-  //     <Route path="/register" element={<Register />}></Route>
-  //   </Routes>
-  // </BrowserRouter>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<ChatRoom />}></Route>
+      <Route path="/login" element={<Login />}></Route>
+      <Route path="/register" element={<Register />}></Route>
+    </Routes>
+  </BrowserRouter>
   );
 }
 
